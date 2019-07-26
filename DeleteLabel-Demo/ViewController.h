7ff68e995b6e04d0d2409de49a488de20d096b47
dong.wang <dong.wang@yingzt.com>
2019-07-26 14:39:11 +0800

    Initial Commit

---
+++ DeleteLabel-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeleteLabel-Demo
+//
+//  Created by 王泽龙 on 2019/7/26.
+//  Copyright © 2019 王泽龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DeleteLabel-Demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DeleteLabel-Demo
+//
+//  Created by 王泽龙 on 2019/7/26.
+//  Copyright © 2019 王泽龙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
